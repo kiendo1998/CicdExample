c8593de702b956a2a3d7828a58166e00c8800b5c
kiendo <kienphule@gmail.com>
2025-06-11 16:02:06 +0700

    update repo

--- src/main/java/org/example/TestController.java
+++ src/main/java/org/example/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
-        return "Spring Boot is working abc!";
+        return "Spring Boot is working!";
     }
 }
